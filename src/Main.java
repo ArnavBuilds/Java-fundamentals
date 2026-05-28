8f1badc46eb13719c21293958d0405743c4779b0
Arnav Sathe <arnavsathe9@gmail.com>
2026-05-28 19:40:44 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[]args){
+        System.out.println("hello world");
+        int arnav=10;
+        int Arnav=11;
+        //System.out.println("arnav"); gives output arnav as
+        // double quotes is consider as string
+        System.out.println(arnav);
+        System.out.println(Arnav);//variables are case sensive in java
+    }
+}
