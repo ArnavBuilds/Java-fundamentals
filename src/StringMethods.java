dac3e8924a437609551dfa846e157d09845e44dc
Arnav Sathe <arnavsathe9@gmail.com>
2026-06-18 20:52:08 +0530

    Add complete code of StringMethods

---
+++ src/StringMethods.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class StringMethods {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        String ab ="  Rahul";
+        System.out.println(ab);
+        System.out.println("Enter username:");
+        String username = sc.nextLine();//prints complete username at once
+        System.out.println("Your username is:"+username);
+        String uppercase= ab.toUpperCase();//prints RAHUL
+        System.out.println("uppercase username:"+uppercase);
+        String trimmed =ab.trim();
+        System.out.println("Trimmed name :"+trimmed);//cuts the spaces trims it .
+        String city ="Nagpur";
+        String substring =city.substring(2,5);
+        System.out.println("words from index 2 to 4 are :"+substring);
+    }
+}
